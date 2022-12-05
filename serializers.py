15421d4ed32f2ed51e1f55544a7715372c17b0f6
mabuni97 <119873105+mabuni97@users.noreply.github.com>
2022-12-05 20:56:23 +0530

    Delete serializers.py

--- serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from export.models import FoodSales
-
-class FoodSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FoodSales
-        exclude = ('id', )
